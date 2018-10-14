ec60d3b0bf366b0d0c504914b911a45d6fcb9c5c
MarcelleBond <mbond@student.wethinkcode.co.za>
2018-10-14 14:42:10 +0200

    fuck this day

--- day09/ex02/todo.js
+++ day09/ex02/todo.js
@@ -1,8 +1,7 @@
 var ptext;
 todolist = [];
 
-//cookies
-//make cookie then write cookie write and clear functions
+
 function setCookie(listval) {
     delcookie();
     var d = new Date();
@@ -12,20 +11,13 @@ function setCookie(listval) {
         document.cookie += "todo=" + todolist[ac] + "%";
     }
     document.cookie += "; expires=" + d;
-   // document.write ("Setting Cookies : " + "name=" + todolist[1]);
-  // console.log(document.cookie);
 }
 
 function delcookie()
 {
-   // ReadCookie();
    var deldate = new Date();
    deldate.setMonth(deldate.getMonth() - 1);
- //   var delcook = "";
     document.cookie += 'test=; expires=Thu, 01 Jan 1970 00:00:00 GMT';
-  //  document.cookie = delcook;
-    //console.log(document.cookie);
-  // ReadCookie();
    
 }
 
@@ -48,7 +40,6 @@ function getCookie(cname) {
 function checkCookie() {
     var tdcook = getCookie("todo");
     if (tdcook != "") {
-        //loadarray into todolist[]
     
         var ca = document.cookie.split('%');
         for(var i=0; i<ca.length - 1; i++){
@@ -71,19 +62,17 @@ function ReadCookie()
         for(var i=0; i<cookiearray.length; i++){
             name = cookiearray[i].split('=')[0];
             value = cookiearray[i].split('=')[1];
-         //  alert("Key : " + name + " Value : " + value);
         }
     }
 
 
 window.onload = function () {
-    //create cookies and save them
     checkCookie();
     newtodobut = document.getElementById("newprompt");
     clrbutt = document.getElementById("clear");
     cookiebut = document.getElementById("cc");
     newtodobut.addEventListener("click", myPromptFunc, false);
-    //delete
+
     clrbutt.addEventListener("click", clearDiv, false);
     cookiebut.addEventListener("click", setCookie, false);
     delc = document.getElementById("delc");
@@ -108,12 +97,9 @@ function myPromptFunc() {
 function divDraw()
 {
     clearDiv();
-    //draw divs
     var ic = 0;
     while(ic < (todolist.length))
     {
-        //alert(todolist.length);
-        //alert(JSON.stringify(todolist));
         var tododiv = document.createElement("div");
         var todo = document.createElement("p");
         var t = document.createTextNode(todolist[ic]);
@@ -123,14 +109,10 @@ function divDraw()
         tododiv.appendChild(todo);
         tododiv.style.backgroundColor = "pink";
 
-        //clear div and redraw from todolist array
         document.getElementById("ft_list").appendChild(tododiv);
         ic++;
     }
-    //setCookie();
     
-  //  console.log(JSON.stringify(todolist));
-   // disPlayAllCookies();
 }
 
 function clearDiv()
@@ -140,14 +122,10 @@ function clearDiv()
     {
         delNode.removeChild(delNode.firstChild);
     }
-  //  console.log(document.cookie);
-    //alert(document.cookie);
-  //  divDraw();
 }
 
 function itemDel(key)
 {
-   // alert("We gettin trid of it" + key);
     if (confirm('Delete task from list?')) {
         var delNode = document.getElementById(key);
         delNode.parentNode.removeChild(delNode);
